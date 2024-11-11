f5d15698b7cd2fc3fc9ab9613a7c9d07bd62a518
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2024-11-11 17:23:39 +0530

    commit

---
+++ Module 5/11-11-API/connect.php
@@ -0,0 +1,11 @@
+<?php
+
+	define('HOST','localhost');
+	define('USER','root');
+	define('PASS','');
+	define('DB','test1');
+
+	$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to connect');
+
+
+?>
---
+++ Module 5/11-11-API/insert.php
@@ -0,0 +1,19 @@
+<?php
+
+	include('connect.php');
+	
+	$name = $_POST["name"];
+	$surname = $_POST["surname"];
+	$email = $_POST["email"];
+
+	if($name=="" && $surname=="" && $email=="")
+	{
+		echo '0';
+	}
+	else
+	{
+		$sql = "insert into info(name,surname,email) values ('$name','$surname','$email')";
+		mysqli_query($con,$sql);
+	}
+
+?>
